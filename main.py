e4da24fa525712cb0ce17279193007248c251b60
Nick <greavesnicholas834@gmail.com>
2024-05-21 12:06:34 +1000

    functions

--- main.py
+++ main.py
@@ -24,3 +24,39 @@ while True:
             print('Incorrect Password')
         else:
             print('Your balance is: ', accountBalance)
+
+    if action == 'd':
+        print('make a deposit')
+        userPassword = input('Please enter your password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            deposit = int(input('Please enter your deposited amount: '))
+            accountBalance = deposit + accountBalance
+            print(f'your balance is now, {accountBalance}.')
+
+    if action == 'w':
+        print('make a withdrawl')
+        userPassword = input('Please enter your password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            withdrawl = int(input('Please enter your withdrawl amount: '))
+            accountBalance = accountBalance - withdrawl
+            print(f'your balance is now, {accountBalance}.')
+
+    if action == 's':
+        print('show account')
+        userPassword = input('please enter your password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            print(f'Account name: {accountName}')
+            print(f'Account balance: {accountBalance}')
+
+    if action == 'q':
+        confirmation = input('Are you sure you want to quit (Y/N): ')
+        if confirmation.upper() == 'Y':
+            exit()
+        else:
+            print('OK')
